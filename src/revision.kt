46b2dc48d530f3f50877d79fcecc34c9f30ac07f
Muhammad <muhammadxasanboyev1234@gmail.com>
2025-08-25 09:15:48 +0500

    val and var

--- src/revision.kt
+++ src/revision.kt
@@ -1,3 +1,11 @@
+//fun main(){
+//    println("Hello world")
+//}
+
 fun main(){
-    println("Hello world")
+    //val is read only variable, we cannot reassign value
+    val readOnlyVariable = 1
+    //var is mutable variable, we can easily reassign new values
+    var mutableVariable = 1
+    mutableVariable = 2
 }
