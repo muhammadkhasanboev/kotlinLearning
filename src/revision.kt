8c8dac969e98b052ca15cb872b7f45e15f4d4098
Muhammad <muhammadxasanboyev1234@gmail.com>
2025-08-25 09:20:36 +0500

    Data types

--- src/revision.kt
+++ src/revision.kt
@@ -2,10 +2,23 @@
 //    println("Hello world")
 //}
 
+//fun main(){
+//    //val is read only variable, we cannot reassign value
+//    val readOnlyVariable = 1
+//    //var is mutable variable, we can easily reassign new values
+//    var mutableVariable = 1
+//    mutableVariable = 2
+//}
+
 fun main(){
-    //val is read only variable, we cannot reassign value
-    val readOnlyVariable = 1
-    //var is mutable variable, we can easily reassign new values
-    var mutableVariable = 1
-    mutableVariable = 2
+    /*Types in Kotlin:
+    * Byte, Short, Int, Long
+    * UByte, UShort, UInt, ULong -> unsigned integers
+    * Float, Double
+    * Boolean
+    * Char
+    * String
+    * */
+    val integerVariable: Int = 1
+    println("this is '$integerVariable' integer variable")
 }
